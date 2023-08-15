003b17af71a4937a69ba3226e1fd523c25fcf6c7
Grausegnom <christian.mintert@mailbox.or>
2023-08-15 22:21:18 +0200

    Populate the HexMap with hexes

--- main.py
+++ main.py
@@ -3,24 +3,18 @@ from visualize_map import MapVisualRepresentation
 
 class Hex:
 
-    def __init__(self, q, r, s):
+    def __init__(self, q, r):
 
-        assert q + r + s == 0, "Invalid cube coordinates"
         self.q = q
         self.r = r
-        self.s = s
+        self.s = -q-r
 
-    def cube_to_axial(self):
+    def get_axial_coord(self):
 
-        q = self.q
-        r = self.r
         return (q,r)
 
-    def axial_to_cube(self):
+    def get_cube_coord(self):
 
-        q = self.q
-        r = self.r
-        s = -self.q-self.r
         return(q,r,s)
 
 class HexMap:
@@ -29,13 +23,26 @@ class HexMap:
 
         self.map={}
 
-    def set_hex (self, hex, value):
-        self.map[hex] = value
+    def print_hexes(self):
+        for hex in self.map.keys():
+            print(f"q: {hex.q}, r: {hex.r}, s:{hex.s}")
+    
+    
+    def initialize_map(self, left, right, top, bottom):
 
-    def get_hex (self, hex):
-        return self.map.get(hex,None)
+        for r in range(top, bottom +1):
+            print(r)
+            r_offset = int (r // 2.0)
+            for q in range(left - r_offset, right - r_offset + 1):
+                print(q)
+                self.map.update({Hex(q, r):None})
 
+        
+    
 
-wraithsong_map = MapVisualRepresentation()
-wraithsong_map.draw_map()
+wraithsong_map = HexMap()
+wraithsong_map.initialize_map(-3,3,-2,2)
+wraithsong_map.print_hexes()
+
+print(wraithsong_map)
 
